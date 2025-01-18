4cdadd916ff1be70b7c35bc9330ac889ffbb1a81
Phurinat Bunyachai <phurinatb@zupports.co>
2025-01-18 16:15:57 +0700

    edit: add provide var

--- src/components/layouts/Header.vue
+++ src/components/layouts/Header.vue
@@ -1,4 +1,7 @@
+<script setup lang="ts">
+import { inject } from "vue";
+const isDesktop = inject("isDesktop");
+</script>
 <template>
-  <div class="h-[50px] bg-yellow-500">d</div>
+  <div class="h-[50px] bg-yellow-500">{{ isDesktop }}</div>
 </template>
-<script setup lang="ts"></script>
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,15 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
-import { createPinia } from 'pinia'
-import { router } from './router'
+import { createApp } from "vue";
+import "./style.css";
+import App from "./App.vue";
+import { createPinia } from "pinia";
+import { router } from "./router";
+import { useMediaQuery } from "@vueuse/core";
 
-const pinia = createPinia()
-const app = createApp(App)
+const pinia = createPinia();
+const app = createApp(App);
+const isDesktop: boolean = useMediaQuery("(min-width: 768px)");
 
-app.use(router)
-app.use(pinia)
-app.mount('#app')
+app.provide("isDesktop", isDesktop);
+app.use(router);
+app.use(pinia);
+app.mount("#app");
